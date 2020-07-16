a70efc1f680ec8faa5da8b7c7be39e954e6eb6e8
manager@bitlancetechhub.com <manager@bitlancetechhub.com>
2020-07-16 18:50:30 +0530

    update helper file and fix datetime issue

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -4,8 +4,8 @@ class Helper
 {
     public static function converToTz($time="")
     {
-        $date = new DateTime($time);
-        $date->setTimezone(new DateTimeZone(config('app.timezone')));
+        $date = new \DateTime($time);
+        $date->setTimezone(new \DateTimeZone(config('app.timezone')));
         $time= $date->format('d M Y h:i A');
         return $time;
     }
